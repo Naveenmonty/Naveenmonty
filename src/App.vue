7d1368e26263967aec4049df39ff4039b7bb0df6
Naveenmonty <naveen08343@gmail.com>
2023-02-15 16:20:45 +0530

    hello

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+   <Gxio></Gxio>
+    </div>
+</template>
+
+<script>
+import Gxio from "../src/components/Rapid.vue";
+export default {
+  name: 'App',
+  components:{
+     Gxio
+  },
+  data(){
+    return{
+    
+    }
+  },
+  computed:{
+filtered(){
+  return this.items.filter(i=>i.body.includes(this.search))
+}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Rapid.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <b-button @click="submitt()" variant="primary">moni</b-button>{{response}}{{list}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name:"RapiD",
+data(){
+    return{
+list:[]
+    }
+},
+mounted(){
+this.submitt();
+},
+methods:{
+    submitt(){
+ axios.get('https://random-d.uk/api/v2').then((response)=>{
+   return this.list= response.data ;
+}).catch((error)=>{
+    console.log(error)
+})
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
